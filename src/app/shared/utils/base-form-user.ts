e356393fa1a2ce21be543d40549e48f170461238
karentarchb <kare.t@montechelo.com.co>
2021-06-20 22:35:22 -0500

    se creo una nueva interfaz

---
+++ src/app/shared/utils/base-form-user.ts
@@ -0,0 +1,47 @@
+import { FormBuilder, Validators } from '@angular/forms';
+import { Injectable } from '@angular/core';
+
+@Injectable({ providedIn: 'root' })
+export class BaseFormUser {
+  private isValidEmail = /\S+@\S+\.\S+/;
+  errorMessage = null;
+
+  constructor(private fb: FormBuilder) {}
+
+  baseForm = this.fb.group({
+    username: [
+      '',
+      [Validators.required, Validators.pattern(this.isValidEmail)],
+    ],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+    role: ['', [Validators.required]],
+  });
+
+  isValidField(field: string): boolean {
+    this.getErrorMessage(field);
+    return (
+      (this.baseForm.get(field).touched || this.baseForm.get(field).dirty) &&
+      !this.baseForm.get(field).valid
+    );
+  }
+
+  private getErrorMessage(field: string): void {
+    const { errors } = this.baseForm.get(field);
+
+    //diseñado por: karen torres
+    //20/06/2021
+    //errores para mostrar en el login
+
+    if (errors) {
+      const minlenght = errors?.minlength?.requiredLength;
+      const messages = {
+        required: 'Escribe aquí, por favor.',
+        pattern: 'No es un correo valido verifique y vuelva a intentar...',
+        minlength: `Deben ser mínimo ${minlenght} caracteres`,
+      };
+
+      const errorKey = Object.keys(errors).find(Boolean);
+      this.errorMessage = messages[errorKey];
+    }
+  }
+}
